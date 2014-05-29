ca0bcb75601a367e3ccefef106e7ee19a5b5fb9c
doroteya-agayna <doroteya.agayna@telerik.com>
2014-05-29 10:12:08 +0300

    Add ContainsExample sample

---
+++ ContainsExample/ContainsExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UsersEntitiesModel;
+using PermissionsEntitiesModel;
+using Telerik.OpenAccess;
+
+namespace ContainsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> userIDsDropDatabase = new List<int>();
+            List<int> userIDsDeleteRecodrds = new List<int>();
+
+            //Retrieve the userIDs
+            using (PermissionsModel dbContext = new PermissionsModel())
+            {
+                //Retrieve the users who can drop the database
+                userIDsDropDatabase = dbContext.Permissions
+                                             .Where(p => p.DBObjectName == "SofiaCarRentalDb" 
+                                                      && p.GrantedPermission == "DROP")
+                                             .Select(p => p.UserID).ToList();
+
+                //Retrieve the users who can delete records from any database table
+                userIDsDeleteRecodrds = dbContext.Permissions
+                                               .Where(p => p.GrantedPermission == "DELETE")
+                                               .Select(p => p.UserID).ToList();
+            }
+
+            using (UsersModel dbContext = new UsersModel())
+            {
+                dbContext.Log = Console.Out;
+                Console.WriteLine("---------------------Contains with IN clause---------------------");
+                List<User> usersDropDatabase = dbContext.Users.Where(u => userIDsDropDatabase.Contains(u.UserID)).ToList();
+
+                Console.WriteLine("\n---------------------Contains with temporary table---------------------");
+                List<User> userDeleteRecords = dbContext.Users.Where(u => userIDsDeleteRecodrds.Contains(u.UserID)).ToList();
+
+                Console.WriteLine("\n---------------------ContainsExtended with temporary table---------------------");
+                userDeleteRecords = dbContext.Users.Where(u => userIDsDeleteRecodrds.ContainsExtended(u.UserID)).ToList();
+
+                Console.WriteLine("\n---------------------Users that can drop the database---------------------");
+                Console.WriteLine("\nName\t\t\t\t\tLast Active On\n");
+                foreach (User user in usersDropDatabase)
+                {
+                    Console.WriteLine("{0}\t\t\t\t{1}", user.UserName, user.LastActiveDate);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ContainsExample/README.md
@@ -0,0 +1,9 @@
+#Bulk Select with Telerik Data Access: ContainsExample#
+
+To run this sample you can execute the next steps:
+
+1. Make sure that the version of **Telerik Data Access** on your side is **Q1 2014 SP1 or higher**.
+2. On the **MS SQL Server** instance on your side, execute the **Permissions.sql** file and the **Users.sql** file. They will create the **Permissions** and the **Users** databases and will populate them with data.
+3. Open the solution in Visual Studio and provide the name of the MS SQL Server instance to the two connection strings in the App.config file of the ContainsExample project.
+4. Upgrade the Telerik Data Access references as described in [this documentation article](http://docs.telerik.com/data-access/upgrade-guide/getting-started-root-migration-tool-upgrade-references).
+5. Run the sample for a test.
